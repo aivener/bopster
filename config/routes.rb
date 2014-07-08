0006ebee9ab3aa8b393f1d3707a89ed904b91182
Sarah Schachman <sarahschachman@college.harvard.edu>
2014-07-08 21:00:22 +0200

    added landing page

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   get '/friendships/:friendship_id/accept_friend' => 'friendships#accept_friend'
 
-
+  get 'pages/landing'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
